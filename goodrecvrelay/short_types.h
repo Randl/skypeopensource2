0b004b624fd31d3bfc56a2d63770e085030a9bc4
Evgenii Zheltonozhskii <zheltonozhskiy@gmail.com>
2016-09-26 17:08:27 +0300

    Removed problematic define (check later)

--- goodrecvrelay/short_types.h
+++ goodrecvrelay/short_types.h
@@ -1,15 +1,23 @@
-#ifndef uint
-#define uint            unsigned int
-#endif
+#ifndef _SHORT_TYPES_SKYPEOPENSOURCE2
+#define _SHORT_TYPES_SKYPEOPENSOURCE2 
+
+#include <stdint.h>
+
+//#ifndef uint
+//#define uint uint_fast32_t  //TODO
+//#endif
 
 #ifndef u8
-#define u8                unsigned char
+#define u8   uint_fast8_t
 #endif
 #ifndef u16
-#define u16                unsigned short
+#define u16  uint_fast16_t
 #endif
 #ifndef u32
-#define u32                unsigned long
+#define u32  uint_fast32_t
+#endif
+#ifndef u64
+#define u64  uint_fast64_t
 #endif
 
-
+#endif
